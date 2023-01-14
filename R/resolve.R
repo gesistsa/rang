0c6119353ba5c9fdf3d39699c522a4dcc9b47850
chainsawriot <chainsawtiney@gmail.com>
2023-01-14 19:22:59 +0100

    use anytime instead

--- R/resolve.R
+++ R/resolve.R
@@ -6,11 +6,11 @@
 ## get the latest version as of date
 ## let's call this output dep_df; basically is a rough version of edgelist
 .get_snapshot_dependencies <- function(pkg = "rtoot", snapshot_date = "2022-12-10") {
-    snapshot_date <- lubridate::ymd(snapshot_date)
+    snapshot_date <- anytime::anytime(snapshot_date, tz = "UTC", asUTC = TRUE)
     search_res <- .search(pkg)
     ## exclude rows where Version, Date, dependecies are NA
     search_res <- search_res[!is.na(search_res$Date) & !is.na(search_res$Version),]
-    search_res$pubdate <- lubridate::ymd(search_res$Date)
+    search_res$pubdate <- anytime::anytime(search_res$Date, tz = "UTC", asUTC = TRUE)
     snapshot_versions <- search_res[search_res$pubdate <= snapshot_date,]
     best_version <- tail(snapshot_versions[order(snapshot_versions$pubdate),], n = 1)
     dependencies <- best_version$dependencies[[1]]
@@ -20,5 +20,12 @@
 ## dep_df is a terminal node if
 ## 1. No Import or Depends
 ## 2. Depends is only R
-## 3. Imports/Depends are only unsearchable base packages
-##  base graphics grDevices methods utils grid ??
+## 3. Imports/Depends are only unsearchable default packages
+## getOption("defaultPackages")
+## "datasets", "utils", "grDevices", "graphics", "stats", "methods"
+
+## dbi <- .get_snapshot_dependencies("DBI")
+## R6 <- .get_snapshot_dependencies("R6")
+## saveRDS(dbi, "tests/testdata/dbi_dep_df.RDS")
+## saveRDS(httr, "tests/testdata/httr_dep_df.RDS")
+## saveRDS(R6, "tests/testdata/R6.RDS")
