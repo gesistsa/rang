1c9ebe025c01f8c93e415856bfb2b439798a5bd9
chainsawriot <chainsawtiney@gmail.com>
2023-01-26 20:50:56 +0100

    add installation ground work

---
+++ R/installation.R
@@ -0,0 +1,72 @@
+##granlist <- readRDS("tests/testdata/graph.RDS")
+
+.determine_installation_order <- function(granlist) {
+    dep <- fastmap::fastmap()
+    version <- fastmap::fastmap()
+    for (gran in granlist$grans) {
+        current_pkg <- unique(gran$original$x)
+        current_ver <- unique(gran$original$x_version)
+        current_dep <- .keep_queryable_dependencies(dep_df = gran$original, no_enhances = gran$no_enhances, no_suggests = gran$no_suggests)
+        dep$set(current_pkg, current_dep)
+        version$set(current_pkg, current_ver)
+        for (dep_df in gran$deps) {
+            current_pkg <- unique(dep_df$x)
+            current_ver <- unique(dep_df$x_version)
+            current_dep <- .keep_queryable_dependencies(dep_df = dep_df, no_enhances = gran$no_enhances, no_suggests = gran$no_suggests)
+            dep$set(current_pkg, current_dep)
+            version$set(current_pkg, current_ver)
+        }
+    }
+    ## installation simulation
+    installed_packages <- c()
+    needed_packages <- dep$keys()
+    ## install all terminal nodes
+    for (package in needed_packages) {
+        if (is.null(dep$get(package))) {
+            installed_packages <- c(installed_packages, package)
+        }
+    }
+    while(length(setdiff(needed_packages, installed_packages)) != 0) {
+        for (package in needed_packages) {
+            ##print(package)
+            if (!package %in% installed_packages) {
+                ## check requirement
+                requirement_fulfilled <- length(setdiff(dep$get(package), installed_packages)) == 0
+                if (requirement_fulfilled) {
+                    installed_packages <- c(installed_packages, package)
+                }
+            }
+        }
+    }
+    vapply(installed_packages, function(x) version$get(x), character(1))
+}
+
+.install_from_cran <- function(x, lib, path = tempdir()) {
+    url <- paste0("https://cran.r-project.org/src/contrib/Archive/", names(x), "/", names(x), "_", x, ".tar.gz")
+    tarball_path <- file.path(path, paste0(names(x), "_", x, ".tar.gz"))
+    tryCatch({
+        suppressWarnings(download.file(url, destfile = tarball_path))
+    }, error = function(e) {
+        ## is the current latest
+        url <- paste0("https://cran.r-project.org/src/contrib/", names(x), "_", x, ".tar.gz")
+        download.file(url, destfile = tarball_path)
+    })
+    install.packages(pkg = tarball_path, lib = lib, repos = NULL)
+    ## check and error
+    installed_packages <- installed.packages(lib.loc = lib)
+    if (!names(x) %in% dimnames(installed_packages)[[1]]) {
+        stop("Fail to install ", names(x), "\n")
+    }
+    invisible()
+}
+
+.consolidate_sysreqs <- function(granlist) {
+    strsplit(granlist$deps_sysreqs, "-y ")
+    debs <- vapply(strsplit(granlist$deps_sysreqs, "-y "), function(x) x[2], character(1))
+    cmd <- paste("apt-get install -y", paste(debs, collapse = " "))
+    if ("default-jdk" %in% debs) {
+        cmd <- paste(cmd, "liblzma-dev libpcre3-dev libbz2-dev && R CMD javareconf")
+    }
+    cmd
+}
+
