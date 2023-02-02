8e69a8b5acfb7649ab3ab51fd9a558bc3c85c083
chainsawriot <chainsawtiney@gmail.com>
2023-02-02 15:51:29 +0100

    Add tests for unresolved_pkgs bug

--- tests/testthat/test_resolve.R
+++ tests/testthat/test_resolve.R
@@ -1,3 +1,36 @@
 test_that("defensive programming", {
     expect_error(resolve("LDAvis", os = "windows"))
 })
+
+## The following are real tests. Even with memoisation, please keep at minimum
+
+test_that("normal", {
+    ## normal case
+    skip_if_offline()
+    skip_on_cran()
+    expect_silent(x2 <- resolve("LDAvis", snapshot_date = "2023-01-01"))
+    expect_equal(x2$unresolved_pkgs, character(0))
+    expect_equal(x2$deps_sysreqs, character(0))
+})
+
+test_that("unresolved", {
+    ## too early for LDAvis
+    skip_if_offline()
+    skip_on_cran()
+    expect_error(res <- .get_snapshot_dependencies("LDAvis", snapshot_date = "2001-10-01"))
+    warns <- capture_warnings(res <- resolve("LDAvis", snapshot_date = "2000-10-01"))
+    expect_true(length(warns) == 2)
+    expect_true(any(grepl("^Some package", warns)))
+    expect_true(any(grepl("^No packages to query", warns)))
+    expect_equal(x$unresolved_pkgs, "LDAvis")
+    expect_equal(x$grans, list())
+    expect_true(is.na(x$deps_sysreqs))
+    ## a mixture; sna is old enough
+    warns2 <- capture_warnings(x3 <- resolve(c("sna", "LDAvis"), snapshot_date = "2001-10-01"))
+    expect_true(length(warns2) == 1)
+    expect_true(any(grepl("^Some package", warns2)))
+    expect_false(any(grepl("^No packages to query", warns2)))
+    expect_equal(x3$unresolved_pkgs, "LDAvis")
+    expect_false(length(x3$grans) == 0)
+    expect_equal(x3$deps_sysreqs, character(0))
+})
