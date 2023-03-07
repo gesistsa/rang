b4f9c9870ca2e3d84f58938d4bd4a876adff0484
chainsawriot <chainsawtiney@gmail.com>
2023-03-07 11:44:08 +0100

    Add peng example

---
+++ paper/peng.sh
@@ -0,0 +1,8 @@
+Rscript -e "devtools::load_all(); dockerize(resolve('ptproc', '2004-07-01'), 'pengdocker', cache = TRUE)"
+docker build -t pengimg ./pengdocker
+docker run -d --rm --name "pengcontainer" -ti pengimg
+docker cp peng.R pengcontainer:/peng.R
+docker exec pengcontainer R CMD BATCH peng.R
+docker exec pengcontainer cat peng.Rout
+docker cp pengcontainer:/peng.Rout peng.Rout
+docker stop pengcontainer
