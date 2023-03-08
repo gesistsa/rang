972e3c8f298e6a3ea6bd58edde3f3b346644095e
chainsawriot <chainsawtiney@gmail.com>
2023-03-08 12:51:38 +0100

    Add first complete draft of the paper

--- paper/oser/oser.R
+++ paper/oser/oser.R
@@ -5,5 +5,5 @@ cran_pkgs <- as_pkgrefs("meta-analysis") ## dmetar is an undeclared github packa
 
 cran_pkgs[cran_pkgs == "cran::dmetar"] <- "MathiasHarrer/dmetar"
 x <- resolve(cran_pkgs, "2021-08-11", verbose = TRUE)
-print(x, all_pkgs = TRUE)
-dockerize(x, "oser_docker", materials_dir = "meta-analysis", cache = TRUE)
+##print(x, all_pkgs = TRUE)
+dockerize(x, "oserdocker", materials_dir = "meta-analysis", cache = TRUE)
