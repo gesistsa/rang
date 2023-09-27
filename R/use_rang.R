5a4e5750f637f5c678adb9052eff3df670098244
chainsawriot <chainsawtiney@gmail.com>
2023-09-27 14:43:08 +0200

    Update doc

--- R/use_rang.R
+++ R/use_rang.R
@@ -59,6 +59,7 @@ use_rang <- function(path = ".", add_makefile = TRUE, add_here = TRUE,
 #' @param path character, path to the project root
 #' @param add_rang logical, whether to run [use_rang()] to `path`
 #' @param ... additional parameters pass to [use_rang()]
+#' @inheritParams use_rang
 #' @return path, invisibly
 #' @seealso [use_rang()]
 #' @details
